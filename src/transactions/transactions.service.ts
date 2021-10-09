91e27e3ba45e1e6db98d60bd2df80c81df3cdc80
Klaus Dellano <klausdell@hotmail.com>
2021-10-09 13:09:55 -0300

    Implementing API REST

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -4,15 +4,15 @@ import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { Transaction } from './entities/transaction.entity';
 @Injectable()
 export class TransactionsService {
-  constructor(
-    @InjectModel(Transaction) private transactionModel: typeof Transaction,
-  ) {}
+    constructor(
+        @InjectModel(Transaction) private transactionModel: typeof Transaction,
+    ) {}
 
-  create(createTransactionDto: CreateTransactionDto) {
-    return this.transactionModel.create(createTransactionDto);
-  }
+    create(createTransactionDto: CreateTransactionDto) {
+        return this.transactionModel.create(createTransactionDto);
+    }
 
-  findAll() {
-    return this.transactionModel.findAll();
-  }
+    findAll() {
+        return this.transactionModel.findAll();
+    }
 }
